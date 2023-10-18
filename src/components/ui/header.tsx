69667f31d5f0bc0e1ffd04fce4f3027391217f57
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-10-17 23:06:16 -0300

    "feat: ui components"

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,18 +1,31 @@
+'use client';
+
 import {
   HomeIcon,
   ListOrderedIcon,
   LogInIcon,
+  LogOutIcon,
   MenuIcon,
   PercentIcon,
   ShoppingCartIcon,
 } from 'lucide-react';
-import { Card } from './card';
 import { Button } from './button';
+import { Card } from './card';
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from './sheet';
+import { signOut, useSession } from 'next-auth/react';
+import { Avatar, AvatarImage } from './avatar';
+import { AvatarFallback } from '@radix-ui/react-avatar';
+import { Separator } from './separator';
 
 const Header = () => {
+  const { status, data } = useSession();
+
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
+
   return (
-    <Card className="flex justify-between p-[1.875rem] items-center">
+    <Card className="flex items-center justify-between p-[1.875rem]">
       <Sheet>
         <SheetTrigger asChild>
           <Button size="icon" variant="outline">
@@ -24,32 +37,82 @@ const Header = () => {
           <SheetHeader className="text-left text-lg font-semibold">
             Menu
           </SheetHeader>
+
+          {status === 'authenticated' && data?.user && (
+            <div className="flex flex-col">
+              <div className="flex items-center gap-2 py-4">
+                <Avatar>
+                  <AvatarFallback>
+                    {data.user.name?.[0].toUpperCase()}
+                  </AvatarFallback>
+
+                  {data.user.image && <AvatarImage src={data.user.image} />}
+                </Avatar>
+
+                <div className="flex flex-col">
+                  <p className="font-medium">{data.user.name}</p>
+                  <p className="text-sm opacity-75">Boas compras!</p>
+                </div>
+              </div>
+
+              <Separator />
+            </div>
+          )}
+
           <div className="mt-4 flex flex-col gap-2">
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <LogInIcon size={16} />
-              Fazer Login
-            </Button>
-            <Button variant="outline" className="w-full justify-start gap-2">
+            {status === 'unauthenticated' && (
+              <Button
+                onClick={() => {
+                  window.location.href = '/login';
+                }}
+                variant="outline"
+                className="w-full justify-start gap-2"
+              >
+                <LogInIcon size={16} />
+                Fazer Login
+              </Button>
+            )}
+
+            {status === 'authenticated' && (
+              <Button
+                onClick={handleLogoutClick}
+                variant="outline"
+                className="w-full justify-start gap-2"
+              >
+                <LogOutIcon size={16} />
+                Fazer Logout
+              </Button>
+            )}
+
+            <Button
+              onClick={() => {
+                window.location.href = '/';
+              }}
+              variant="outline"
+              className="w-full justify-start gap-2"
+            >
               <HomeIcon size={16} />
-              Inicio
+              Início
             </Button>
+
             <Button variant="outline" className="w-full justify-start gap-2">
               <PercentIcon size={16} />
               Ofertas
             </Button>
+
             <Button variant="outline" className="w-full justify-start gap-2">
               <ListOrderedIcon size={16} />
-              Catalago
+              Catálogo
             </Button>
           </div>
         </SheetContent>
       </Sheet>
 
-      <h1>
-        <span className="text-primary font-bold">FSW</span> Store
+      <h1 className="text-lg font-semibold">
+        <span className="text-primary">FSW</span> Store
       </h1>
 
-      <Button>
+      <Button size="icon" variant="outline">
         <ShoppingCartIcon />
       </Button>
     </Card>
