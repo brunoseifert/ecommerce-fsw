07c0b2c11d657db5306751f5329230e74b0b80bb
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-10-17 23:07:26 -0300

    "feat: form login"

---
+++ src/components/user-login-auth.tsx
@@ -0,0 +1,137 @@
+'use client';
+
+import React, { useState } from 'react';
+import { cn } from '@/lib/utils';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Icons } from '@/components/icons';
+import { signIn } from 'next-auth/react';
+import { useToast } from '@/components/ui/use-toast';
+import { ToastAction } from '@/components/ui/toast';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+interface IUser {
+  email: string;
+  password: string;
+}
+
+export function UserLoginForm({ className, ...props }: UserAuthFormProps) {
+  const [data, setData] = useState<IUser>({ email: '', password: '' });
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const { toast } = useToast();
+
+  const router = useRouter();
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const res = await signIn<'credentials'>('credentials', {
+      ...data,
+      redirect: false,
+    });
+
+    if (res?.error) {
+      toast({
+        title: 'Erro ao entrar',
+        description: res.error,
+        variant: 'destructive',
+        action: (
+          <ToastAction altText="Tente novamente">Tente novamente</ToastAction>
+        ),
+      });
+    } else {
+      toast({
+        title: 'Sucesso',
+        description: 'Você entrou com sucesso',
+        action: <ToastAction altText="OK">OK</ToastAction>,
+      });
+      router.push('/');
+    }
+
+    setData({ email: '', password: '' });
+    setIsLoading(false);
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  return (
+    <div className={cn('grid gap-6 m-10', className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+              name="email"
+              value={data.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              id="password"
+              placeholder="senha"
+              type="password"
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isLoading}
+              name="password"
+              value={data.password}
+              onChange={handleChange}
+            />
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && <Icons.spinner className="mr-2" />}
+            Entrar
+          </Button>
+        </div>
+      </form>
+      <h5 className="flex flex-row items-center justify-center gap-1 font-light">
+        Não tem uma conta?<Link href="/register"> Registre-se</Link>
+      </h5>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Ou continue com
+          </span>
+        </div>
+      </div>
+      <Button
+        onClick={() => signIn('google', { callbackUrl: '/' })}
+        variant="outline"
+        type="button"
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.google className="mr-2 h-4 w-4" />
+        )}{' '}
+        Google
+      </Button>
+    </div>
+  );
+}
