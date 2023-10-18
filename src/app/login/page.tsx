5ed37883ca5397acb1633e1670487f39438597b1
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-10-17 23:03:06 -0300

    "feat:route login"

---
+++ src/app/login/page.tsx
@@ -0,0 +1,46 @@
+import { Metadata } from 'next';
+
+import Link from 'next/link';
+import { Command } from 'lucide-react';
+import { UserLoginForm } from '@/components/user-login-auth';
+
+export const metadata: Metadata = {
+  title: 'Autenticação',
+  description:
+    'Autenticação utilizando Shadcs-ui Next-Auth (Authjs), prisma e Nextjs 13.4+',
+};
+
+export default async function Register() {
+  return (
+    <div className="py-48 md:py-0  h-[800px] flex-col items-center justify-center md:grid lg:px-0">
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">Entrar</h1>
+            <p className="text-sm text-muted-foreground">
+              Entre com seus dados de Login
+            </p>
+          </div>
+          <UserLoginForm />
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{' '}
+            <Link
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Termos de Serviço
+            </Link>{' '}
+            e{' '}
+            <Link
+              href="/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Política de Privacidade
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
