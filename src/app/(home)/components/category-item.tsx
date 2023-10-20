6e2952b799025d59eaf87204cdbe350e20e17eaa
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-10-20 11:00:49 -0300

    chore: category item button

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,35 +1,23 @@
-import { Badge } from '@/components/ui/badge';
-import { Category } from '@prisma/client';
-import {
-  HeadphonesIcon,
-  KeyboardIcon,
-  MonitorIcon,
-  MouseIcon,
-  SpeakerIcon,
-  SquareIcon,
-} from 'lucide-react';
+import { Badge } from "@/components/ui/badge";
+import { CATEGORY_ICON } from "@/constants/category-icon";
+import { Category } from "@prisma/client";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
 }
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
-  const categoryIcon = {
-    keyboards: <KeyboardIcon />,
-    monitors: <MonitorIcon />,
-    headphones: <HeadphonesIcon />,
-    mousepads: <SquareIcon />,
-    speakers: <SpeakerIcon />,
-    mouses: <MouseIcon />,
-  };
   return (
-    <Badge
-      variant="outline"
-      className="py-3 flex justify-center items-center gap-2 rounded-lg"
-    >
-      {categoryIcon[category.slug as keyof typeof categoryIcon]}
-      <span className="font-semibold text-sx">{category.name}</span>
-    </Badge>
+    <Link href={`/category/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="flex items-center justify-center gap-2 rounded-lg py-3"
+      >
+        {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+        <span className="text-xs font-bold">{category.name}</span>
+      </Badge>
+    </Link>
   );
 };
 
