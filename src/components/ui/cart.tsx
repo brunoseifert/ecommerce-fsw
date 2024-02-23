abbafca688f4180c37c357ce9fb0687f03d11018
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-02-23 11:43:04 -0300

    style: gap in down components

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -7,7 +7,7 @@ import CartItem from "./cart-item";
 const Cart = () => {
   const { products } = useContext(CartContext);
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <Badge
         className="w-fit gap-1 border-r-2 border-primary px-3 py-1 text-sm font-semibold uppercase"
         variant="outline"
