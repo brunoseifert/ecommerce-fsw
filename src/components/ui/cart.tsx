6f334f931c490a8fc6491283a0ff0b59bc995734
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-02-23 22:16:00 -0300

    feat: add stripe checkout and server actions

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -4,11 +4,23 @@ import { Badge } from "./badge";
 import { ShoppingCartIcon } from "lucide-react";
 import CartItem from "./cart-item";
 import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
+import { createCheckout } from "@/actions/checkout";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
+
+  const handleFinishPurchaseClick = async () => {
+    const checkout = await createCheckout(products);
+
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    stripe?.redirectToCheckout({ sessionId: checkout.id });
+  };
   return (
-    <div className="flex w-full flex-col gap-4">
+    <div className="flex h-full w-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-r-2 border-primary px-3 py-1 text-sm font-semibold uppercase"
         variant="outline"
@@ -16,16 +28,20 @@ const Cart = () => {
         <ShoppingCartIcon size={24} />
         carrinho
       </Badge>
-      <div>
-        {products.length > 0 ? (
-          products.map((product) => (
-            <CartItem key={product.id} product={product} />
-          ))
-        ) : (
-          <p className="font-semibold">
-            Você ainda não tem nenhum produto no carrinho!
-          </p>
-        )}
+      <div className="flex h-full flex-col gap-5">
+        <ScrollArea className="h-[600px]">
+          <div className="fkex h-full flex-col">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem key={product.id} product={product} />
+              ))
+            ) : (
+              <p className="font-semibold">
+                Você ainda não tem nenhum produto no carrinho!
+              </p>
+            )}
+          </div>
+        </ScrollArea>
       </div>
       <div className="flex flex-col gap-3">
         <Separator />
@@ -51,6 +67,12 @@ const Cart = () => {
           <p>Total</p>
           <p>R$ {total.toFixed(2)}</p>
         </div>
+        <Button
+          className="text-xs font-semibold uppercase"
+          onClick={handleFinishPurchaseClick}
+        >
+          finalizar compra
+        </Button>
       </div>
     </div>
   );
