cd788bd4f567bf6621e4afad8aef766445afbe8b
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-02-27 17:35:40 -0300

    chore: bargde percentIcon

--- src/app/deals/page.tsx
+++ src/app/deals/page.tsx
@@ -1,6 +1,8 @@
+import { Badge } from "@/components/ui/badge";
 import ProductItem from "@/components/ui/product-item";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { prismaClient } from "@/lib/prisma";
+import { PercentIcon } from "lucide-react";
 
 const DealsPage = async () => {
   const deals = await prismaClient.product.findMany({
@@ -13,6 +15,13 @@ const DealsPage = async () => {
 
   return (
     <div className="p-5">
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant="outline"
+      >
+        <PercentIcon size={16} />
+        Ofertas
+      </Badge>
       <div className="grid grid-cols-2 gap-8">
         {deals.map((product) => (
           <ProductItem
