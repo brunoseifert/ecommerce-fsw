b7cb4b8f29641a76e4111ef335e287ac2be94c30
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-02-28 17:39:41 -0300

    feat: add create order server action

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,10 @@
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { AuthOptions } from 'next-auth';
-import { prismaClient } from './prisma';
-import GoogleProvider from 'next-auth/providers/google';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import { prismaClient } from "./prisma";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prismaClient),
@@ -14,44 +14,55 @@ export const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     CredentialsProvider({
-      name: 'Credentials',
+      name: "Credentials",
       credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
-        email: { label: 'Email', type: 'email' },
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+        email: { label: "Email", type: "email" },
       },
       async authorize(credentials, req): Promise<any> {
-        console.log('Authorize method', credentials);
+        console.log("Authorize method", credentials);
 
         if (!credentials?.email || !credentials.password)
-          throw new Error('Dados de logins necessários');
+          throw new Error("Dados de logins necessários");
 
         const user = await prismaClient.user.findUnique({
           where: { email: credentials.email },
         });
 
         if (!user || !user.hashedPassword) {
-          throw new Error('Usuário não registrado atraves de credenciais');
+          throw new Error("Usuário não registrado atraves de credenciais");
         }
 
         const matchPassword = await bcrypt.compare(
           credentials.password,
-          user.hashedPassword
+          user.hashedPassword,
         );
         if (!matchPassword) {
-          throw new Error('Senha inválida');
+          throw new Error("Senha inválida");
         }
 
         return user;
       },
     }),
   ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user = { ...session.user, id: user.id } as {
+        id: string;
+        name: string;
+        email: string;
+      };
+
+      return session;
+    },
+  },
   session: {
-    strategy: 'jwt',
+    strategy: "jwt",
   },
   secret: process.env.SECRET!,
-  debug: process.env.NODE_ENV === 'development',
+  debug: process.env.NODE_ENV === "development",
   pages: {
-    signIn: '/',
+    signIn: "/",
   },
 };
